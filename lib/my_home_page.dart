4d3d05b7dd76028667fb06d7959b6372279d2232
Eric Schlichting <ericschlichting@gmail.com>
2024-04-07 08:45:15 -0500

    Start ui

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,8 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:shaderify/shaderify.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -16,16 +13,34 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final tts = FlutterTts();
   int _counter = 0;
-  
-  late FragmentShader program;
 
   Future<void> _incrementCounter() async {
-    program = (await FragmentProgram.fromAsset('shaders/furspace.sksl')).fragmentShader();
     await tts.speak("Hello World");
     setState(() {
       _counter++;
     });
   }
+
+// say "Welcome to your daily practice Eric." -v $VOICE
+// say "Take a deep breath and settle in." -v $VOICE
+// sleep 16
+
+// say "Now we will do some mindfulness. Choose mindful breathing or wise mind meditation." -v $VOICE
+// sleep 180
+
+// say "Now we will do deep relaxation. Choose cue-controlled relaxation, band of light, safe-place visualization, or progressive muscle relaxation." -v $VOICE
+// sleep 180
+
+// say "Now we will do self observation. Choose thought defusion, judgement defusion,  or be mindful of emotions without judgement." -v $VOICE
+// sleep 180
+
+// say "Now we will do affirmation. Some examples are I am learning every day, I can do this, I have grown so much, I am doing the best I can, I love the people in my life, I love me, or My actions are in line with my values." -v $VOICE
+// sleep 180
+
+// say "Select a committed action. Some examples are physical health, read self help, be assertive about a problem, use problem solving, spend quality time with kids, do something you enjoy, use beginner's mind, use radical acceptance, use FLAME, improve your physical space, use REST." -v $VOICE
+// sleep 20
+
+// say "Well done Eric, you have completed your daily practice. See you tomorrow." -v $VOICE
 
   @override
   Widget build(BuildContext context) {
@@ -34,30 +49,34 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-          const Positioned.fill(child: ShaderWidget(shaderPath: 'shaders/furspace.frag')),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            buildTextField('First Name'),
+            buildTextField('Last Name'),
+            buildTextField('Mindfulness Meditation'),
+            buildTextField('Deep Relaxation'),
+            buildTextField('Committed Action'),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+    );
+  }
+
+  Widget buildTextField(String labelText) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          labelText,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: labelText,
+          ),
+        ),
+      ],
     );
   }
 }
